084cf141ffeeb4a5baa2b092fe5972941f5bc399
dubhunter <get@willmason.me>
2013-02-02 12:28:24 -0800

    sms voting app

---
+++ dbdApp/controllers/Hackathon.php
@@ -0,0 +1,20 @@
+<?php
+class Stashboard extends TSController {
+
+	protected function init() {
+		$this->noRender();
+	}
+
+	public function doDefault() {
+		dbdLog($this->getParams());
+		$from = $this->getParam('From');
+		$vote = (int) $this->getParam('Body');
+
+		if (Vote::getCount(array('from' => $from)) == 0) {
+			$V = new Vote();
+			$V->setFrom($from);
+			$V->setVote($vote);
+			$V->save();
+		}
+	}
+}
---
+++ dbdApp/models/Vote.php
@@ -0,0 +1,5 @@
+<?php
+class Vote extends dbdModel {
+	const TABLE_NAME = 'votes';
+	const TABLE_KEY = 'vote_id';
+}
