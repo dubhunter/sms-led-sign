9aeff47cc0a592cdfe341eb6df00bac648492b2d
dubhunter <get@willmason.me>
2013-02-02 12:32:08 -0800

    ensure ints

--- dbdApp/controllers/Hackathon.php
+++ dbdApp/controllers/Hackathon.php
@@ -8,9 +8,9 @@ class Hackathon extends TSController {
 	public function doDefault() {
 		dbdLog($this->getParams());
 		$from = $this->getParam('From');
-		$vote = (int) $this->getParam('Body');
+		$vote = $this->getParam('Body');
 
-		if (Vote::getCount(array('from' => $from)) == 0) {
+		if (is_numeric($vote) && Vote::getCount(array('from' => $from)) == 0) {
 			$V = new Vote();
 			$V->setFrom($from);
 			$V->setVote($vote);
