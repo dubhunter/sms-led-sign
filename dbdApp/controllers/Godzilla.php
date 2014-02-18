b15472bdb399bbddda5a152bfefec31ff11200ed
dubhunter <get@willmason.me>
2014-02-17 19:48:47 -0800

    make sure we load twilio

--- dbdApp/controllers/Godzilla.php
+++ dbdApp/controllers/Godzilla.php
@@ -2,6 +2,7 @@
 class Godzilla extends TSController {
 
 	public function doDefault() {
+		$twilio	= self::getTwilioClient();
 		$numbers = array(
 			TWILIO_NUMBER_SIGN,
 			TWILIO_NUMBER_HEADSUP,
@@ -12,7 +13,7 @@ class Godzilla extends TSController {
 				'To' => $number,
 				'Body' => 'GODZILLA!!!',
 			);
-			$response = self::getTwilioClient()->request('/' . TWILIO_VERSION . '/Accounts/' . TWILIO_ACCOUNT_SID . '/SMS/Messages', 'POST', $sms);
+			$response = $twilio->request('/' . TWILIO_VERSION . '/Accounts/' . TWILIO_ACCOUNT_SID . '/SMS/Messages', 'POST', $sms);
 			echo json_encode($response, JSON_PRETTY_PRINT);
 		}
 	}
